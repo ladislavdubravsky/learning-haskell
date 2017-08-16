06e30392232db82738474c25e6918a928974925c
Ladislav Dubravský <30551080+ldubravsky@users.noreply.github.com>
2017-08-16 20:41:35 +0200

    Create 10-interval-scheduling.hs

---
+++ CG/02-medium/10-interval-scheduling.hs
@@ -0,0 +1,21 @@
+import System.IO
+import Control.Monad
+import qualified Data.Map.Strict as M
+import Data.List (sort)
+
+type Task = (Int,Int)
+
+processInp :: String -> Task
+processInp = (\[x,y] -> (x + y,x)) . map (read :: String -> Int) . words
+
+cntTasks :: [Task] -> Int -> Int
+cntTasks [] _              = 0
+cntTasks (t:ts) minEndTime = if snd t >= minEndTime
+                             then 1 + cntTasks ts (fst t)
+                             else cntTasks ts minEndTime
+
+main = do
+    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
+    n <- readLn :: IO Int
+    tasks <- sort <$> (replicateM n $ fmap processInp getLine)
+    print $ cntTasks tasks 0
